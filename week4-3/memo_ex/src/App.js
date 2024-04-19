9852ee336d8770bf36b28be9ea84d7edbe4e7eef
yoonza <zoony1110@sch.ac.kr>
2024-04-19 21:17:08 +0900

    add study projec files 4-3

--- week4-3/memo_ex/src/App.js
+++ week4-3/memo_ex/src/App.js
@@ -16,7 +16,56 @@ const MemoComponent = ({a,b,c}) => {
       <p>더한 값: {memorizedValue} </p>
     </div>
   )
+};
+
+// 컴포넌트 트리구조
+const ParentsComponent = () => {
+  const [name, setName] = React.useState('perl');
+
+  console.log('ParentsComponent 렌더링');
+  return (
+    <div>
+      <h1>부모 컴포넌트</h1>
+      <ChildOne name={name}/>
+      <ChildTwo/>
+      <ChildThree/>
+
+      <p>{name}</p>
+
+      <button onClick={() => {
+        setName(name+'a');
+      }}>이름 바꾸기</button>
+    </div>
+ );
 }
+
+const ChildOne = (name) => {
+  console.log('ChildOne 렌더링');
+  return (
+    <div>
+      <h1>자식1 컴포넌트</h1>
+    </div>
+  );
+}
+
+const ChildTwo = React.memo(() => { // React.memo(함수 내용)
+  console.log('ChildTwo 렌더링');
+  return (
+    <div>
+      <h1>자식2 컴포넌트</h1>
+    </div>
+  );
+})
+
+const ChildThree = () => {
+  console.log('ChildThree 렌더링');
+  return (
+    <div>
+      <h1>자식3 컴포넌트</h1>
+    </div>
+  );
+}
+
 function App() {
   const [numbers, setNumbers] = React.useState({
     a: 0,
@@ -33,6 +82,10 @@ function App() {
       <button onClick={() => {
         setNumbers({...numbers, c:numbers.c+1});
       }}>c를 변경해</button>
+
+      <hr/>
+
+      <ParentsComponent/>
     </div>
   );
 }
